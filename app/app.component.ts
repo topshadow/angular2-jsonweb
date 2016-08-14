286b2df71b22af9f4434d5b3606059ac7850e5df
topshadow <731502029@qq.com>
2016-08-14 13:26:41 +0800

    first

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: 'this is AppComponent'
+})
+export class AppComponent implements OnInit {
+    ngOnInit(){
+
+    }
+}
